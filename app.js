27f3fde407d9484633a86be3d2c618ca83a5c84e
Lauren Vo <laurennt.vo@gmail.com>
2026-04-04 22:24:16 -0700

    Update app.js

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ let chartTooltip = null;
 let popupRef = null;
 let serviceLayer = null;
 
-const ASSISTANT_API_URL = 'http://127.0.0.1:8001/api/chat';
+const ASSISTANT_API_URL = 'https://my-yoi-backend.onrender.com/api/chat';
 
 function clearTransientUi() {
   if (chartTooltip) chartTooltip.style('opacity', 0);
